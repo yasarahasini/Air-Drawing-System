fff5b98bd32ef3e73d88ccfd6588d66614b1819b
Yasarahasini <yasarahasini61@gmail.com>
2026-04-14 18:57:59 +0530

    update pages

--- app/lib/handTracking.ts
+++ app/lib/handTracking.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Camera from "@/components/Camera";
+import Camera from "../components/camera";
 
 const HandTracking = () => {
   const [handData, setHandData] = useState<any>(null);
